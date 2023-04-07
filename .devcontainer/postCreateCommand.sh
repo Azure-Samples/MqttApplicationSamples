3cb738890e0895b37109fe503143819801b1b137
Rido <rido-min@users.noreply.github.com>
2023-04-07 15:52:59 +0000

    fix apt mosquitto

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,5 @@
 # Install mosquitto client
-sudo apt-get update && sudo apt-get install mosquitto-clients && sudo apt-get install mosquitto -y
+sudo apt-get update && sudo apt-get install mosquitto-clients mosquitto -y
 
 #Install step cli
 wget https://dl.step.sm/gh-release/cli/docs-cli-install/v0.23.4/step-cli_0.23.4_amd64.deb
