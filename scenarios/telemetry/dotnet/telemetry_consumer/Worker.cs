08dbac60154c7c249d819d2579c1909232141f80
ridomin <ridomin@live.com>
2023-04-28 09:44:57 -0700

    upd Telemetry, remove RX

--- scenarios/telemetry/dotnet/telemetry_consumer/Worker.cs
+++ scenarios/telemetry/dotnet/telemetry_consumer/Worker.cs
@@ -1,10 +1,7 @@
-using GeoJSON.Text.Geometry;
 using MQTTnet;
 using MQTTnet.Client;
 using MQTTnet.Client.Extensions;
-using MQTTnet.Extensions.External.RxMQTT.Client;
 using MQTTnet.Extensions.ManagedClient;
-using System.Reactive.Linq;
 
 namespace telemetry_consumer;
 
@@ -25,17 +22,20 @@ public class Worker : BackgroundService
         var cs = ConnectionSettings.CreateFromEnvVars(_configuration.GetValue<string>("envFile")!);
         _logger.LogInformation("Connecting to {cs}", cs);
 
-        var mqttClient = new MqttFactory().CreateRxMqttClient(MqttNetTraceLogger.CreateTraceLogger());
+        var mqttClient = new MqttFactory().CreateManagedMqttClient(MqttNetTraceLogger.CreateTraceLogger());
 
         mqttClient.InternalClient.ConnectedAsync += async cea =>
         {
-            _logger.LogWarning("Client {ClientId} connected: {ResultCode}", mqttClient.InternalClient.Options.ClientOptions.ClientId, cea.ConnectResult.ResultCode);
+            _logger.LogWarning("Client {ClientId} connected: {ResultCode}", mqttClient.InternalClient.Options.ClientId, cea.ConnectResult.ResultCode);
 
-            new TelemetryRx<Point>(mqttClient)
-                .Start("vehicles/+/position")
-                .Subscribe(m => _logger.LogInformation("Received msg from {id}. Coordinates lat: {x}, lon: {y}", 
-                    m.ClientIdFromTopic, m.Payload!.Coordinates.Latitude, m.Payload.Coordinates.Longitude));
-
+            PositionTelemetryConsumer poisitionTelemetry = new(mqttClient.InternalClient)
+            {
+                OnTelemetryReceived = m => 
+                _logger.LogInformation("Received msg from {id}. Coordinates lat: {x}, lon: {y}",
+                        m.ClientIdFromTopic, 
+                        m.Payload!.Coordinates.Latitude, 
+                        m.Payload.Coordinates.Longitude)
+            };
             await Task.Yield();
         };
 
