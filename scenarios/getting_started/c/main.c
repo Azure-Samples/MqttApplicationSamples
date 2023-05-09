56e37d1efdca1ccb5933a68f410ca30d27383496
Valerie Avva <vaavva@microsoft.com>
2023-05-09 16:52:06 -0700

    getting started error checking and ctrl c cleanup

--- scenarios/getting_started/c/main.c
+++ scenarios/getting_started/c/main.c
@@ -1,6 +1,7 @@
 /* Copyright (c) Microsoft Corporation. All rights reserved. */
 /* SPDX-License-Identifier: MIT */
 
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,6 +16,14 @@
 #define SUB_TOPIC "sample/+"
 #define QOS 1
 #define MQTT_VERSION MQTT_PROTOCOL_V311
+
+static volatile sig_atomic_t keep_running = 1;
+
+static void sig_handler(int _)
+{
+  (void)_;
+  keep_running = 0;
+}
 
 /* Callback called when the client receives a CONNACK message from the broker and we want to
  * subscribe on connect. */
@@ -32,23 +41,43 @@ void on_connect_with_subscribe(
   /* Making subscriptions in the on_connect() callback means that if the
    * connection drops and is automatically resumed by the client, then the
    * subscriptions will be recreated when the client reconnects. */
-  result = mosquitto_subscribe_v5(mosq, NULL, SUB_TOPIC, QOS, 0, NULL);
-
-  if (result != MOSQ_ERR_SUCCESS)
+  if ((result = mosquitto_subscribe_v5(mosq, NULL, SUB_TOPIC, QOS, 0, NULL)) != MOSQ_ERR_SUCCESS)
   {
     printf("Error subscribing: %s\n", mosquitto_strerror(result));
     /* We might as well disconnect if we were unable to subscribe */
-    mosquitto_disconnect_v5(mosq, reason_code, props);
+    if ((result = mosquitto_disconnect_v5(mosq, reason_code, props)) != MOSQ_ERR_SUCCESS)
+    {
+      printf("Error disconnecting: %s\n", mosquitto_strerror(result));
+    }
   }
+}
+
+void cleanup(
+    struct mosquitto* mosq,
+    mqtt_client_obj* obj,
+    mqtt_client_connection_settings* connection_settings)
+{
+  if (mosq != NULL)
+  {
+    mosquitto_disconnect_v5(mosq, MOSQ_ERR_SUCCESS, NULL);
+    mosquitto_loop_stop(mosq, false);
+    mosquitto_destroy(mosq);
+  }
+  mosquitto_lib_cleanup();
+  free(connection_settings);
+  free(obj);
 }
 
 /*
  * This sample sends and receives messages to/from the Broker. X509 authentication is used.
+ * @return MOSQ_ERR_SUCCESS (0) on success, other enum mosq_err_t on failure
  */
 int main(int argc, char* argv[])
 {
+  signal(SIGINT, sig_handler);
+
   struct mosquitto* mosq;
-  int result = 0;
+  int result = MOSQ_ERR_SUCCESS;
   mqtt_client_connection_settings* connection_settings
       = calloc(1, sizeof(mqtt_client_connection_settings));
 
@@ -56,48 +85,45 @@ int main(int argc, char* argv[])
   obj->print_message = NULL;
   obj->mqtt_version = MQTT_VERSION;
 
-  mosq = mqtt_client_init(true, argv[1], on_connect_with_subscribe, obj, connection_settings);
-  result = mosquitto_connect_bind_v5(
-      mosq,
-      connection_settings->hostname,
-      connection_settings->tcp_port,
-      connection_settings->keep_alive_in_seconds,
-      NULL,
-      NULL);
-
-  if (result != MOSQ_ERR_SUCCESS)
+  if ((mosq = mqtt_client_init(true, argv[1], on_connect_with_subscribe, obj, connection_settings))
+      == NULL)
   {
-    mosquitto_destroy(mosq);
+    result = MOSQ_ERR_UNKNOWN;
+  }
+  else if (
+      (result = mosquitto_connect_bind_v5(
+           mosq,
+           connection_settings->hostname,
+           connection_settings->tcp_port,
+           connection_settings->keep_alive_in_seconds,
+           NULL,
+           NULL))
+      != MOSQ_ERR_SUCCESS)
+  {
     printf("Connection Error: %s\n", mosquitto_strerror(result));
-    return 1;
+    result = MOSQ_ERR_UNKNOWN;
   }
-
-  result = mosquitto_loop_start(mosq);
-
-  if (result != MOSQ_ERR_SUCCESS)
+  else if ((result = mosquitto_loop_start(mosq)) != MOSQ_ERR_SUCCESS)
   {
-    mosquitto_destroy(mosq);
     printf("loop Error: %s\n", mosquitto_strerror(result));
-    return 1;
+    result = MOSQ_ERR_UNKNOWN;
   }
-
-  while (true)
+  else
   {
-    result = mosquitto_publish_v5(
-        mosq, NULL, PUB_TOPIC, (int)strlen(PAYLOAD), PAYLOAD, QOS, false, NULL);
-
-    if (result != MOSQ_ERR_SUCCESS)
+    while (keep_running)
     {
-      printf("Error publishing: %s\n", mosquitto_strerror(result));
-    }
+      result = mosquitto_publish_v5(
+          mosq, NULL, PUB_TOPIC, (int)strlen(PAYLOAD), PAYLOAD, QOS, false, NULL);
 
-    sleep(5);
+      if (result != MOSQ_ERR_SUCCESS)
+      {
+        printf("Error publishing: %s\n", mosquitto_strerror(result));
+      }
+
+      sleep(5);
+    }
   }
 
-  mosquitto_loop_stop(mosq, true);
-
-  mosquitto_destroy(mosq);
-  mosquitto_lib_cleanup();
-  free(connection_settings);
-  return 0;
+  cleanup(mosq, obj, connection_settings);
+  return result;
 }
