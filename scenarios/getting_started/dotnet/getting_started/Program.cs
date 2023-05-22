62b5923f4f82f2df8d1f18ff27ff5bacf84761e9
Rido <rido-min@users.noreply.github.com>
2023-05-22 16:19:15 -0700

    review dotnet qos (#39)

--- scenarios/getting_started/dotnet/getting_started/Program.cs
+++ scenarios/getting_started/dotnet/getting_started/Program.cs
@@ -16,10 +16,10 @@ Console.WriteLine($"Client Connected: {mqttClient.IsConnected} with CONNACK: {co
 mqttClient.ApplicationMessageReceivedAsync += async m => await Console.Out.WriteAsync(
     $"Received message on topic: '{m.ApplicationMessage.Topic}' with content: '{m.ApplicationMessage.ConvertPayloadToString()}'\n\n");
 
-var suback = await mqttClient.SubscribeAsync("sample/+");
+var suback = await mqttClient.SubscribeAsync("sample/+", MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce);
 suback.Items.ToList().ForEach(s => Console.WriteLine($"subscribed to '{s.TopicFilter.Topic}'  with '{s.ResultCode}'"));
 
-var puback = await mqttClient.PublishStringAsync("sample/topic1", "hello world!");
+var puback = await mqttClient.PublishStringAsync("sample/topic1", "hello world!", MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce);
 Console.WriteLine(puback.ReasonString);
 
 Console.ReadLine();
