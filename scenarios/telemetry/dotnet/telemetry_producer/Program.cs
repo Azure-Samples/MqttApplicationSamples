61cc02549bcea9b4f6430e2b895ea42b97cf4a6b
ridomin <ridomin@live.com>
2023-06-07 10:11:33 -0700

    enable traces

--- scenarios/telemetry/dotnet/telemetry_producer/Program.cs
+++ scenarios/telemetry/dotnet/telemetry_producer/Program.cs
@@ -1,6 +1,6 @@
 using telemetry_producer;
 
-//System.Diagnostics.Trace.Listeners.Add(new System.Diagnostics.ConsoleTraceListener());
+System.Diagnostics.Trace.Listeners.Add(new System.Diagnostics.ConsoleTraceListener());
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
