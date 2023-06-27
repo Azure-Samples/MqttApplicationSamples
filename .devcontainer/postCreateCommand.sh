6e9cf8e4f22251b1c6eecafa1113a4cb48c921de
vaavva <vaavva@microsoft.com>
2023-06-27 14:29:27 -0500

    add protobuf library to codespaces

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,6 @@
 # Install mosquitto client
 sudo apt-add-repository ppa:mosquitto-dev/mosquitto-ppa -y
-sudo apt-get update && sudo apt-get install mosquitto-clients mosquitto ninja-build libmosquitto-dev uuid-dev libjson-c-dev -y
+sudo apt-get update && sudo apt-get install mosquitto-clients mosquitto ninja-build libmosquitto-dev uuid-dev libjson-c-dev libprotobuf-c-dev -y
 
 #Install step cli
 wget https://github.com/smallstep/cli/releases/download/v0.24.4/step-cli_0.24.4_amd64.deb
