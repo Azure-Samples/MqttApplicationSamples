ea8c3c7084e2a9186a14472348e1aa249f4c1631
Nick Patilsen <npatilsen@microsoft.com>
2023-10-26 15:00:12 -0700

    boilerplate

---
+++ scenarios/getting_started/go/getting_started.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	//"github.com/eclipse/paho.golang/paho"
+)
+
+func main() {
+	fmt.Println("hello from go!")
+}
+// find env file
+
+// initialize client
+
+// connect
+
+// subscribe/suback
+
+// publish/puback
+
+// disconnect
---
+++ scenarios/getting_started/go/go.mod
@@ -0,0 +1,7 @@
+module getting_started
+
+go 1.21.3
+
+require github.com/eclipse/paho.golang v0.12.0
+
+require golang.org/x/sync v0.4.0 // indirect
