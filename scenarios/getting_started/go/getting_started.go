9896a9ceb790c0e77ba9aae8ccd227e12f7e4a83
Nick Patilsen <npatilsen@microsoft.com>
2023-11-02 15:25:52 -0700

    asdf

--- scenarios/getting_started/go/getting_started.go
+++ scenarios/getting_started/go/getting_started.go
@@ -1,71 +1,153 @@
 package main
 
 import (
-	"bufio"
 	"context"
 	"crypto/tls"
-	"flag"
+	"crypto/x509"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"log"
-	"net"
+	"net/url"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/eclipse/paho.golang/autopaho"
 	"github.com/eclipse/paho.golang/paho"
-	"github.com/joho/godotenv"
+	//"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("hello from go!")
-	stdin := bufio.NewReader(os.Stdin)
+	//stdin := bufio.NewReader(os.Stdin)
 
-	// Load connection settings via environment variables and set connection parameters
-	godotenv.Load("../.env")
+	/*
+		// Load connection settings via environment variables and set connection parameters
+		godotenv.Load("../.env")
 
-	server := flag.String("server", fmt.Sprintf("%s:%s", os.Getenv("MQTT_HOST_NAME"), "8883"), "The full URL of the MQTT server to connect to")
-	topic := "sample/+"
-	qos := flag.Int("qos", 0, "The QoS to send the messages at")
-	retained := flag.Bool("retained", false, "Are the messages sent with the retained flag")
-	clientId := os.Getenv("MQTT_CLIENT_ID")
-	username := os.Getenv("MQTT_USERNAME")
-	password := "YPC/ETXxvltLdv+hHupUCczQSc8Te4wtwSHx1P49Qxs="
-	flag.Parse()
+		server := flag.String("server", fmt.Sprintf("%s:%s", os.Getenv("MQTT_HOST_NAME"), "8883"), "The full URL of the MQTT server to connect to")
+		topic := "sample/+"
+		qos := flag.Int("qos", 0, "The QoS to send the messages at")
+		//retained := flag.Bool("retained", false, "Are the messages sent with the retained flag")
+		clientId := os.Getenv("MQTT_CLIENT_ID")
+		//username := os.Getenv("MQTT_USERNAME")
+		//password := "YPC/ETXxvltLdv+hHupUCczQSc8Te4wtwSHx1P49Qxs="
+		flag.Parse()*/
 
-	conn, err := net.Dial("tcp", *server)
+	// Load certificates
+	cert, err := tls.LoadX509KeyPair("../sample_client.pem", "../sample_client.key")
 	if err != nil {
-		log.Fatalf("Failed to connect to %s: %s", *server, err)
+		log.Fatal(err)
 	}
 
-	tlsConfig := tls.Config{
-		Certificates: nil,
+	caCert, _ := ioutil.ReadFile("../chain.pem")
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	u, err := url.Parse("mqtt://npatilsen-eventgrid.westus2-1.ts.eventgrid.azure.net:8883")
+	if err != nil {
+		panic(err)
 	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	cfg := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		ClientCAs:    caCertPool,
+	}
+
+	// is this needed?
+	/*
+			listener, err := tls.Listen("tcp", "npatilsen-eventgrid.westus2-1.ts.eventgrid.azure.net:8883", cfg)
+			if err != nil {
+				log.Fatal(err)
+			}
+			_ = listener
+
+
+		fmt.Println("dialing eventgrid")
+		conn, err := net.Dial("tcp", "npatilsen-eventgrid.westus2-1.ts.eventgrid.azure.net:8883")
+		if err != nil {
+			log.Fatalf("Failed to connect to %s: %s", "npatilsen-eventgrid.westus2-1.ts.eventgrid.azure.net:8883", err)
+		}
+	*/
 
 	clientConfig := autopaho.ClientConfig{
-		TlsCfg: &tlsConfig,
+		BrokerUrls: []*url.URL{u},
+		TlsCfg:     cfg,
+		KeepAlive:  20,
+		OnConnectionUp: func(cm *autopaho.ConnectionManager, connAck *paho.Connack) {
+			fmt.Println("mqtt connection up")
+			if _, err := cm.Subscribe(context.Background(), &paho.Subscribe{
+				Subscriptions: []paho.SubscribeOptions{
+					{Topic: "sample/+", QoS: 1},
+				},
+			}); err != nil {
+				fmt.Printf("failed to subscribe (%s). This is likely to mean no messages will be received!\n", err)
+			}
+			fmt.Println("mqtt subscription made")
+		},
+		OnConnectError: func(err error) { fmt.Printf("error whilst attempting connection: %s\n", err) },
 		ClientConfig: paho.ClientConfig{
-			Conn: conn,
+			ClientID: "sample_client",
+			Router: paho.NewSingleHandlerRouter(func(m *paho.Publish) {
+				fmt.Printf("received message on topic %s; body: %s (retain: %t)\n", m.Topic, m.Payload, m.Retain)
+			}),
+			OnClientError: func(err error) { fmt.Printf("server requested disconnect: %s\n", err) },
+			OnServerDisconnect: func(d *paho.Disconnect) {
+				if d.Properties != nil {
+					fmt.Printf("server requested disconnect: %s\n", d.Properties.ReasonString)
+				} else {
+					fmt.Printf("server requested disconnect; reason code: %d\n", d.ReasonCode)
+				}
+			},
 		},
 	}
 
-	// TODO -- make into autopaho?
-	c := paho.NewClient(clientConfig)
-
-	properties := paho.ConnectProperties{
-		AuthData:   nil,
-		AuthMethod: "Intermediate01",
+	fmt.Println("attempting to connect")
+	c, err := autopaho.NewConnection(ctx, clientConfig) // starts process; will reconnect until context cancelled
+	if err != nil {
+		panic(err)
+	}
+	// Wait for the connection to come up
+	if err = c.AwaitConnection(ctx); err != nil {
+		panic(err)
 	}
 
-	cp := &paho.Connect{
-		KeepAlive:  30,
-		ClientID:   clientId,
-		CleanStart: true,
-		Properties: &properties,
-		Username:   username,
-		Password:   []byte(password),
+	fmt.Println("connection successful!")
+
+	// Publish
+	if _, err = c.Publish(ctx, &paho.Publish{
+		QoS:     1,
+		Topic:   "sample/+",
+		Payload: []byte("TestMessage"),
+	}); err != nil {
+		panic(err)
 	}
+	fmt.Println("published!!!")
+
+	<-ctx.Done() // Wait for user to trigger exit
+	fmt.Println("signal caught - exiting")
+}
+
+/*
+		c := paho.NewClient(clientConfig)
+
+		properties := paho.ConnectProperties{
+			AuthData:   nil,
+			AuthMethod: "Intermediate01",
+		}
+
+		cp := &paho.Connect{
+			KeepAlive:  30,
+			ClientID:   clientId,
+			CleanStart: true,
+			Properties: &properties,
+			Username:   username,
+			Password:   []byte(password),
+		}
+
 
 	cp.UsernameFlag = true
 
@@ -112,8 +194,4 @@ func main() {
 		}
 		log.Println("sent")
 	}
-
-}
-
-// do we have pub and sub?
-// disconnect
+*/
--- scenarios/getting_started/go/go.mod
+++ scenarios/getting_started/go/go.mod
@@ -2,9 +2,10 @@ module getting_started
 
 go 1.21.3
 
-require (
-	github.com/eclipse/paho.golang v0.12.0
-	github.com/joho/godotenv v1.5.1
-)
+require github.com/eclipse/paho.golang v0.12.0
 
-require golang.org/x/sync v0.4.0 // indirect
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sync v0.4.0 // indirect
+)
