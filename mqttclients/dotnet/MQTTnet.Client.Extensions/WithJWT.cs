1e7183fb95d99fd7e9f08a4250a721171631c299
rido-min <rido-min@users.noreply.github.com>
2023-11-30 15:02:28 -0800

    add timer to jwt

--- mqttclients/dotnet/MQTTnet.Client.Extensions/WithJWT.cs
+++ mqttclients/dotnet/MQTTnet.Client.Extensions/WithJWT.cs
@@ -1,27 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
 namespace MQTTnet.Client.Extensions
 {
     public static partial class MqttNetExtensions
     {
-        public static MqttClientOptionsBuilder WithJWT(this MqttClientOptionsBuilder builder, MqttConnectionSettings cs, byte[] token)
+        static Func<byte[]> _getTokenCallBack = null!;
+        static Timer _refreshTimer = null!;
+
+
+        public static MqttClientOptionsBuilder WithJWT(this MqttClientOptionsBuilder builder, MqttConnectionSettings cs, Func<byte[]> getTokenCallBack, IMqttClient mqttClient, TimeSpan refreshPeriod)
         {
+            _getTokenCallBack = getTokenCallBack;
+
             builder
                 .WithTcpServer(cs.HostName, cs.TcpPort)
                 .WithClientId(cs.ClientId)
                 .WithProtocolVersion(Formatter.MqttProtocolVersion.V500)
-                .WithAuthentication("OAUTH2-JWT", token);
+                .WithAuthentication("OAUTH2-JWT", getTokenCallBack());
 
             if (cs.UseTls)
             {
                 builder.WithTlsOptions(new MqttClientTlsOptions() { UseTls = true });
             }
-
+            
+            _refreshTimer = new Timer(RefreshToken, mqttClient, Convert.ToInt32(refreshPeriod.TotalMilliseconds), Convert.ToInt32(refreshPeriod.TotalMilliseconds));
             return builder;
+        }
+
+        static void RefreshToken(object? state)
+        {
+            IMqttClient mqttClient = (MqttClient)state!;
+            if (mqttClient.IsConnected)
+            {
+                Console.WriteLine("Refreshing Token " + DateTime.Now.ToString("o"));
+
+                Task.Run(async () =>
+                {
+                    await mqttClient.SendExtendedAuthenticationExchangeDataAsync(
+                        new MqttExtendedAuthenticationExchangeData()
+                        {
+                            AuthenticationData = _getTokenCallBack(),
+                            ReasonCode = MQTTnet.Protocol.MqttAuthenticateReasonCode.ReAuthenticate
+                        });
+                });
+            }
         }
     }
 }
--- scenarios/jwt_authentication/dotnet/jwt_authentication/Program.cs
+++ scenarios/jwt_authentication/dotnet/jwt_authentication/Program.cs
@@ -9,15 +9,13 @@ MqttConnectionSettings cs = MqttConnectionSettings.CreateFromEnvVars();
 
 IMqttClient mqttClient = new MqttFactory().CreateMqttClient(MqttNetTraceLogger.CreateTraceLogger());
 MqttClientConnectResult connAck = await mqttClient!.ConnectAsync(new MqttClientOptionsBuilder()
-    .WithJWT(cs, GetToken())
+    .WithJWT(cs, GetToken, mqttClient, TimeSpan.FromMinutes(60))
     .Build());
-
-Timer refreshTimer = new Timer(RefreshToken, mqttClient, 5000, 10 * 60 * 1000);
 
 Console.WriteLine($"Client Connected: {mqttClient.IsConnected} with CONNACK: {connAck.ResultCode} with auth method {mqttClient.Options.AuthenticationMethod}");
 
-mqttClient.ApplicationMessageReceivedAsync += async m => await Console.Out.WriteAsync(
-    $"Received message on topic: '{m.ApplicationMessage.Topic}' with content: '{m.ApplicationMessage.ConvertPayloadToString()}'\n\n");
+mqttClient.ApplicationMessageReceivedAsync += async m => await Console.Out.WriteAsync(m.PacketIdentifier.ToString() + ".");
+   // $"Received message on topic: '{m.ApplicationMessage.Topic}' with content: {m.ApplicationMessage.ConvertPayloadToString()}");
 
 MqttClientSubscribeResult suback = await mqttClient.SubscribeAsync("sample/+", MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce);
 suback.Items.ToList().ForEach(s => Console.WriteLine($"subscribed to '{s.TopicFilter.Topic}'  with '{s.ResultCode}'"));
@@ -26,8 +24,7 @@ int counter = 0;
 while (true)
 {
     MqttClientPublishResult puback = await mqttClient.PublishStringAsync("sample/topic1", "hello world!" + counter++, MQTTnet.Protocol.MqttQualityOfServiceLevel.AtLeastOnce);
-    Console.WriteLine(puback.ReasonString);
-    await Task.Delay(10000);
+    await Task.Delay(100);
 }    
 
 static byte[] GetToken()
@@ -36,20 +33,3 @@ static byte[] GetToken()
     AccessToken jwt = defaultCredential.GetToken(new TokenRequestContext(new string[] { "https://eventgrid.azure.net/.default" }));
     return Encoding.UTF8.GetBytes(jwt.Token);
 }
-
-void RefreshToken(object? state)
-{
-    Console.WriteLine("Refreshing Token " + DateTime.Now.ToString("o") );
-    IMqttClient mqttClient = (MqttClient)state!;
-    Task.Run(async () =>
-    {
-        await mqttClient.SendExtendedAuthenticationExchangeDataAsync(
-            new MqttExtendedAuthenticationExchangeData() 
-            {
-                AuthenticationData = GetToken(), 
-                ReasonCode = MQTTnet.Protocol.MqttAuthenticateReasonCode.ReAuthenticate
-            });
-    });
-}
-
-
