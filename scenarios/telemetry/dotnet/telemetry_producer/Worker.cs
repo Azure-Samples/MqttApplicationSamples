18db6b841a2c8b8ef631301ddb814b2edc34851f
Rido <rido-min@users.noreply.github.com>
2024-03-31 07:46:07 +0000

    retries

--- scenarios/telemetry/dotnet/telemetry_producer/Worker.cs
+++ scenarios/telemetry/dotnet/telemetry_producer/Worker.cs
@@ -1,7 +1,9 @@
+using System.Diagnostics;
 using GeoJSON.Text.Geometry;
 using MQTTnet;
 using MQTTnet.Client;
 using MQTTnet.Client.Extensions;
+using MQTTnet.Exceptions;
 
 namespace telemetry_producer;
 
@@ -9,6 +11,7 @@ public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
     private readonly IConfiguration _configuration;
+    private int maxRetryCount = 10;
     public Worker(ILogger<Worker> logger, IConfiguration configuration)
     {
         _logger = logger;
@@ -21,10 +24,33 @@ public class Worker : BackgroundService
         _logger.LogInformation("Connecting to {cs}", cs);
 
         IMqttClient mqttClient = new MqttFactory().CreateMqttClient(MqttNetTraceLogger.CreateTraceLogger());
-        mqttClient.DisconnectedAsync += e =>
+        mqttClient.ConnectedAsync += c => 
         {
-            _logger.LogInformation("Mqtt client disconnected with reason: {e}", e.Reason);
+            _logger.LogInformation("Client connected. {c}", c.ConnectResult.ResultCode);
             return Task.CompletedTask;
+        };
+        mqttClient.DisconnectedAsync += async e =>
+        {
+            _logger.LogWarning("Mqtt client disconnected with reason: {e}, wasConnected {w}", e.Reason, e.ClientWasConnected);
+
+            while (maxRetryCount > 0 && !mqttClient.IsConnected)
+            {
+                if (maxRetryCount == 1)
+                {
+                    _logger.LogCritical("Max Retries, exiting");
+                    Environment.Exit(-1);
+                }
+
+                int delay = (10 - maxRetryCount--) * 1000;
+                _logger.LogWarning("Retrying to reconnect, attempt: {c}, delay {d}", 10 - maxRetryCount, delay);
+                await Task.Delay(delay);
+                await mqttClient.ReconnectAsync(stoppingToken);
+                if (mqttClient.IsConnected)
+                {
+                    maxRetryCount = 10;
+                }
+                
+            }
         };
 
         MqttClientConnectResult connAck = await mqttClient.ConnectAsync(new MqttClientOptionsBuilder().WithConnectionSettings(cs).Build(), stoppingToken);
@@ -35,11 +61,18 @@ public class Worker : BackgroundService
 
         while (!stoppingToken.IsCancellationRequested)
         {
-            MqttClientPublishResult pubAck = await telemetryPosition.SendTelemetryAsync(
-                new Point(new Position(51.899523, -2.124156)), stoppingToken);
-            _logger.LogInformation("Message published with PUBACK {code} and mid {mid}", pubAck.ReasonCode, pubAck.PacketIdentifier);
+            if (mqttClient.IsConnected)
+            {
+                MqttClientPublishResult pubAck = await telemetryPosition.SendTelemetryAsync(
+                    new Point(new Position(51.899523, -2.124156)), stoppingToken);
+                _logger.LogInformation("Message published with PUBACK {code} and mid {mid}", pubAck.ReasonCode, pubAck.PacketIdentifier);
+            }
+            else
+            {
+                _logger.LogError("Client is not connected, missing one message");
+            }
             await Task.Delay(5000, stoppingToken);
-        }
 
+        }
     }
 }
